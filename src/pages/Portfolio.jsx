0786ea12cc8c5d12b3353bef9af94837e3bc6747
Patipat003 <28613patipat@gmail.com>
2025-03-26 10:57:37 +0700

    Commit git

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/layout/header";
+
+const Portfolio = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Portfolio</h1>
+        </div>
+    );
+}
+
+export default Portfolio;
